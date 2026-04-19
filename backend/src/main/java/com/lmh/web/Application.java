e2d88b5708769aa184e4cafcd9fc058d19b2eb84
nguyenhuuloc <nguyenhuuloc.122004@gmail.com>
2026-04-19 22:23:56 +0700

    add .env

--- backend/src/main/java/com/lmh/web/Application.java
+++ backend/src/main/java/com/lmh/web/Application.java
@@ -1,5 +1,6 @@
 package com.lmh.web;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +12,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(Application.class, args);
 	}
 
